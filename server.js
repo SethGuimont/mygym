3c908be94b77e39d58543875669502ce935b7df2
SethGuimont <ur4634zc@metrostate.edu>
2021-03-15 16:24:25 -0500

    HTML changes

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 //load express and start app
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 //const session = require('express-session');
 
 
 //Middleware
 const app = express();
 
-//app.use('/', userRoute);
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+
+
 app.listen(3001, () => {
     console.log('Express server started at port :3001')
 });
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + './public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
